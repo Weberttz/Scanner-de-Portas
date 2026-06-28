cd9547797a2b18909607f3a59aa4d237ac5a13e1
Weberttz <johngaldencio18@gmail.com>
2026-06-28 20:27:54 -0300

    feat: adiciona versão 1.0 do scanner de portas

---
+++ scanner.py
@@ -0,0 +1,30 @@
+import socket
+import time 
+
+def scan_porta(host, porta):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # criar socket Ipv4 + TCP
+    sock.settimeout(1) # tempo de um segundo
+    resultado = sock.connect_ex((host, porta)) # tentar abrir conexão
+    sock.close() # fechar socket
+    return resultado == 0
+
+def main():
+    host = "localhost"
+    portas = range(1, 1025)
+
+    print(f"Escaneando {host}...\n")
+    inicio = time.time()
+
+    for porta in portas:
+        resultado = scan_porta(host, porta)
+
+        if resultado == 0:
+            print(f"[ ABERTA ] porta {porta} ")
+        else:
+            print(f"[ FECHADA ] porta {porta}")
+
+    fim = time.time()
+
+    print(f"\nConcluído em {fim - inicio:.2f}s") #transformar tempo de segundos legíveis
+
+main()
