daa99ab6e466e03255af4bd153affa22a4aaa479
Weberttz <johngaldencio18@gmail.com>
2026-06-28 21:15:58 -0300

    feat: adiciona scanner_v2.py com utilização de Threads

--- scanner_v2.py
+++ scanner_v2.py
@@ -1,14 +1,19 @@
 import socket
 import time 
+from threading import Thread
+
+portas_abertas = []
 
 def scan_porta(host, porta):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # criar socket Ipv4 + TCP
     sock.settimeout(1) # tempo de um segundo
     resultado = sock.connect_ex((host, porta)) # tentar abrir conexão
     sock.close() # fechar socket
-    return resultado == 0
+    if resultado == 0:
+        portas_abertas.append(porta) # adiciona se estiver aberta
 
 def main():
+    threads = []
     host = "localhost"
     portas = range(1, 1025)
 
@@ -16,15 +21,17 @@ def main():
     inicio = time.time()
 
     for porta in portas:
-        resultado = scan_porta(host, porta)
+        thread = Thread(target=scan_porta, args=(host, porta)) # abre nova thread 
+        threads.append(thread) # adiciona à lista de threads
+        thread.start()  # dispara a thread 
 
-        if resultado == 0:
-            print(f"[ ABERTA ] porta {porta} ")
-        else:
-            print(f"[ FECHADA ] porta {porta}")
+    # Espera todas as threads terminarem
+    for thread in threads:
+        thread.join()
 
     fim = time.time()
 
+    print(sorted(portas_abertas)) # ordena a lista de portas abertas
     print(f"\nConcluído em {fim - inicio:.2f}s") #transformar tempo de segundos legíveis
 
 main()
